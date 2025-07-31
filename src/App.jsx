7bdb2fe3ac34ee5ba58039ecc30af21ea7a22a6d
BamithaEkanayake <136635825+BamithaEkanayake@users.noreply.github.com>
2025-07-31 20:49:44 +0530

    fix: format codebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import "./index.css";
 import Quiz from "./components/quiz";
 
 function App() {
-  
-  return  <div className="app-container">
-    <h1>Quiz App</h1>
-
-    <Quiz/>
-    
-  </div>;
-  
+  return (
+    <div className="app-container">
+      <h1>Quiz App</h1>
+      <Quiz />
+    </div>
+  );
 }
 
-export default App
+export default App;
